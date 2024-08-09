4e9eb8c5b6bf2f65e4728860e57804bf2e185ec0
reezazahedi <158621790+reezazahedi@users.noreply.github.com>
2024-08-09 16:03:21 +0330

    Update producer.py

--- producer/producer.py
+++ producer/producer.py
@@ -1,1 +1,23 @@
+import pika
+import time
+import random
 
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='letterbox')
+
+messageId = 1
+
+while(True):
+    message = f"Sending messageId: {messageId}"
+
+    channel.basic_publish(exchange='', routing_key='letterbox', body=message)
+
+    print(f"send message: {message}")
+
+    time.sleep(random.randint(1, 4))
+
+    messageId+=1
+    
